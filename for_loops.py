0bf06db36f53df947f48812fc724dd7789affa92
RonTheHuman <1ron.kokotov@gmail.com>
2022-01-26 21:56:23 +0200

    c

--- for_loops.py
+++ for_loops.py
@@ -4,7 +4,11 @@ import os
 #main change 1
 =======
 #rebase change 1
+<<<<<<< HEAD
 >>>>>>> 2204d98 (a)
+=======
+#rebase change 2
+>>>>>>> c87bc68 (c)
 for x in "Eran Please forgive me for sending this in late as well":
     print(x)
 #main change 2
